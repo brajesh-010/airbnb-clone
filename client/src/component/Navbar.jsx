d84dd7edc2329724158d5b112925984f3d602aea
brajesh_010 <brajeshdas34@outlook.com>
2024-05-01 10:26:30 +0530

    Changed name

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                         d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
                     />
                 </svg>
-                <h1 className="font-bold text-4xl">airbnb</h1>
+                <h1 className="font-bold text-4xl">Airstay</h1>
             </Link>
 
             <div
